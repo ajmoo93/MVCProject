f8127480e5e03036fc24687d709c97d279594775
emil lindgren <hoganas_1937_70@hotmail.com>
2016-11-27 19:21:18 +0100

    fixing some styles and controllers

--- MVC_1/MVC_1/Controllers/AlbumController.cs
+++ MVC_1/MVC_1/Controllers/AlbumController.cs
@@ -9,18 +9,22 @@ namespace MVC_1.Controllers
 {
     public class AlbumController : Controller
     {
+        //Gör en ny Lista av AlbumModel
         public static List<AlbumModel> albume = new List<AlbumModel>();
 
         // GET: Album
         public ActionResult Index()
         {
+            //Retunerar Listan Albume
             return View(albume);
         }
         public ActionResult CreateAlbum(AlbumModel album)
         {
+            //Här skapar vi ett album
             albume.Add(album);
-            return View();
+            return View(album);
         }
+        //En konstruktor som tar hans om nya album
         public AlbumController()
         {
             if (!albume.Any())
@@ -29,11 +33,15 @@ namespace MVC_1.Controllers
                 albume.Add(new AlbumModel { AlbumID = Guid.NewGuid(), AlbumName = "Wobbely", pic = new List<MyModel>(), albumCom = new List<Comment> { new Comment { CommentAlbum = "Tigers are beautiful" } } });
             }
         }
+        //Funktion för att lägga till foto i Album
         public ActionResult AddPhoto()
         {
+            //Skapar upp en ny Lista
             var Mod = new Albumphoto();
+            //för var picture i listan så tar vi från homekontrollern (Listan db Med bilder)
             Mod.picmod = HomeController.db;
             Mod.albumphoto = AlbumController.albume;
+            //sen retunerar vi den nya Listan
             return View(Mod);
 
         }
@@ -53,7 +61,7 @@ namespace MVC_1.Controllers
             var a = albume.FirstOrDefault(x => x.AlbumID == id);
             return View(albume);
         }
-        [HttpPost ActionName("Delete")]
+        [HttpPost ActionName("DeleteAlbum")]
         public ActionResult DeleteAlbumConferm(Guid id)
         {
             var i = albume.FirstOrDefault(x => x.AlbumID == id);
