fa8df763e1f3c71722043145abb3f6a0000cc6b3
Emil lindgren <hoganas_1937_70@hotmail.com>
2016-11-29 15:45:32 +0100

    wow it works

--- MVC_inlamning/MVC_inlamning/Controllers/AdressController.cs
+++ MVC_inlamning/MVC_inlamning/Controllers/AdressController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using MVC_inlamning.Models;
 using System.Web.Mvc;
 
+
 namespace MVC_inlamning.Controllers
 {
     public class AdressController : Controller
@@ -17,11 +18,11 @@ namespace MVC_inlamning.Controllers
         }
         public AdressController()
         {
-            if (!Person.Any())
-            {
-                Person.Add(new PersonModel { PId = Guid.NewGuid(), Name = " Emil", AdressName = "Plingplongvägen", PhoneNumber = "075845758", date = DateTime.Now });
-                Person.Add(new PersonModel { PId = Guid.NewGuid(), Name = "Tilda", AdressName = "Görgenvägen", PhoneNumber = "078454585", date = DateTime.Now });
-            }
+            //if (!Person.Any())
+            //{
+            //    Person.Add(new PersonModel { PId = Guid.NewGuid(), Name = " Emil", AdressName = "Plingplongvägen", PhoneNumber = "075845758", date = DateTime.Now });
+            //    Person.Add(new PersonModel { PId = Guid.NewGuid(), Name = "Tilda", AdressName = "Görgenvägen", PhoneNumber = "078454585", date = DateTime.Now });
+            //}
         }
         public ActionResult ShowPerson()
         {
@@ -33,11 +34,12 @@ namespace MVC_inlamning.Controllers
             //var P = new PersonModel();
             //Person.Add(P);
             return View();
-        }
+        } 
         [HttpPost]
-        public ActionResult NewPersonCon( PersonModel pers)
-           
+        public ActionResult NewPerson(PersonModel pers)           
         {
+            //var lahkneNyLahkme = Person.First(x => x.PId == pers.PId);
+            pers.PId = Guid.NewGuid();
             if (!ModelState.IsValid)
             {
                 //returnerar new person
@@ -48,21 +50,37 @@ namespace MVC_inlamning.Controllers
             //retunerar en lista av person
             return PartialView("List", Person);
         }
-        //public ActionResult Delete(Guid id)
-        //{
-        //    var i = Person.FirstOrDefault(x => x.PId == id);
-        //    return View(i);
-        //}
-        //public ActionResult DeleteCon(Guid id)
-        //{
-        //    var i = Person.FirstOrDefault(x => x.PId == id);
-        //    Person.Remove(i);
-        //    return RedirectToAction("NewPerson");
-        //}
-        //public ActionResult Edit()
-        //{
-            
-        //    return View();
-        //}
+
+        public ActionResult List()
+        {
+            return PartialView("List", Person);
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteCon(Guid id)
+        {
+            var i = Person.FirstOrDefault(x => x.PId == id);
+            Person.Remove(i);
+            return View("NewPerson");
+
+        }
+        public ActionResult Edit(Guid id)
+        {
+            var pers = Person.First(x => x.PId == id);
+            return View(pers);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PersonModel person)
+        {
+            var pers = Person.FirstOrDefault(x => x.PId == person.PId);
+            pers.Name = person.Name;
+            pers.AdressName = person.AdressName;
+            pers.PhoneNumber = person.PhoneNumber;
+            pers.date = person.date;
+                
+            return PartialView("List", pers);
+        }
     }
 }
